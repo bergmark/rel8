ceea2ae8470ca17c8b13252b2e68405fed7c4cf5
Shane O'Brien <shane.obrien@circuithub.com>
2021-06-19 12:06:48 +0100

    `eval` is just `values . pure`

--- src/Rel8/Query/Evaluate.hs
+++ src/Rel8/Query/Evaluate.hs
@@ -6,30 +6,17 @@ module Rel8.Query.Evaluate
 where
 
 -- base
-import Prelude
-
--- opaleye
-import qualified Opaleye.Internal.PackMap as Opaleye
-import qualified Opaleye.Internal.PrimQuery as Opaleye
-import qualified Opaleye.Internal.QueryArr as Opaleye
-import qualified Opaleye.Internal.Tag as Opaleye
-import qualified Opaleye.Internal.Unpackspec as Opaleye
+import Prelude ()
 
 -- rel8
 import Rel8.Expr ( Expr )
-import Rel8.Query ( Query( Query ) )
+import Rel8.Query ( Query )
+import Rel8.Query.Values ( values )
 import Rel8.Table ( Table )
-import Rel8.Table.Opaleye ( unpackspec )
 
 
 -- | 'eval' takes expressions that could potentially have side effects and
 -- \"runs\" them in the 'Query' monad. The returned expressions have no
 -- side effetcs and can safely be reused.
 eval :: Table Expr a => a -> Query a
-eval a = Query $ Opaleye.QueryArr $ \(_, query, tag) ->
-  let
-    tag' = Opaleye.next tag
-    (a', bindings) = Opaleye.run $
-      Opaleye.runUnpackspec unpackspec (Opaleye.extractAttr "eval" tag') a
-  in
-    (a', Opaleye.Rebind True bindings query, tag')
+eval a = values [a]
